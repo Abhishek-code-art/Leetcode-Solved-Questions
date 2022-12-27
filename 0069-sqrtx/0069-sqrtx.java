30545bf8c228b2981197e24942495f1b63e826be
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2022-12-27 17:23:44 +0530

    Time: 112 ms (5.02%), Space: 39.8 MB (73.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)(Math.sqrt(x));
+        if(x == 1) return 1;
+        if(x == 2147483647) return 46340;
+        int i = 0;
+        for(;i <= x/2 && i*i <= Integer.MAX_VALUE; i++) {
+            if(i*i == x) return i;
+            if(i*i < x) {
+                
+            } else {
+                return (i-1);
+            }
+        }
+        return i-1;
     }
 }
