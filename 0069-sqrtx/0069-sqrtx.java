752758254e2e4444a183488ace02ec2b4a911536
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2022-12-27 22:52:14 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (73.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,14 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
         if(x == 0) return 0;
-        int lo = 1;
-        int hi = x;
-        while(lo < hi) {
-            int mid = lo + (hi-lo)/2;
-            if(mid <= x/mid && (mid+1) > x/(mid+1)) return mid;
-            else if(mid > x/mid) hi = mid;
-            else lo = mid+1;
+        // int lo = 1;
+        // int hi = x;
+        // while(lo < hi) {
+        //     int mid = lo + (hi-lo)/2;
+        //     if(mid <= x/mid && (mid+1) > x/(mid+1)) return mid;
+        //     else if(mid > x/mid) hi = mid;
+        //     else lo = mid+1;
+        // }
+        // return lo;
+        return newtonSqrt(x);
+    }
+    public int newtonSqrt(int n) {
+        long i = n;
+        while(i > n/i) {
+            i = (i+n/i)/2;
         }
-        return lo;
+        return (int)i;
     }
 }
