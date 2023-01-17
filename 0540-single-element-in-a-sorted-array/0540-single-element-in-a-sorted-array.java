b13b38b27775f1b38bb74a513eb1da25af77a0ec
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-17 21:45:02 +0530

    Time: 0 ms (100.00%), Space: 48.5 MB (54.58%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,16 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length == 1) return nums[0];
         int lo = 0;
         int hi = nums.length-1;
-        while(lo < hi) {
-            int mid = lo + (hi-lo)/2;
-            if(((mid&1) == 0 && nums[mid] == nums[mid+1]) || ((mid&1) == 1 && nums[mid] == nums[mid-1])) {
-                lo = mid+1;
-            } else {
-                hi = mid;
-            }
+        int ans = binarySearch(nums, lo, hi);
+        return ans;
+    }
+    
+    public int binarySearch(int[] nums, int lo, int hi) {
+        if(lo >= hi) return nums[lo];
+        
+        int mid = (lo+hi)/2;
+        if(((mid&1) == 0 && nums[mid] == nums[mid+1]) || ((mid&1) == 1 && nums[mid] == nums[mid-1])) {
+            return binarySearch(nums, mid+1, hi);
+        } else {
+            return binarySearch(nums, lo, mid);
         }
-        return nums[lo];
     }
 }
