44635b31492aaee6c6ec5af90c13f040094acc4b
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-17 23:06:10 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (98.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = new int[2];
-        ans[0] = firstOcc(nums, target);
-        ans[1] = lastOcc(nums, target);
+        if(nums.length==0) {
+            ans[0] = -1;
+            ans[1] = -1;
+            return ans;
+        }
+        int lo = 0;
+        int hi = nums.length-1;
+        ans[0] = firstOcc(nums, target, lo, hi);
+        ans[1] = lastOcc(nums, target, lo, hi);
         return ans;
     }
     
-    public int firstOcc(int[] arr, int key) {
-        int n = arr.length;
-        int lo = 0;
-        int hi = arr.length-1;
-        int ans = -1;
-        while(lo <= hi) {
-            int mid = (lo + hi)/2;
-            if(arr[mid] == key) {
-                ans = mid;
-                hi = mid - 1;
-            } else if(arr[mid] < key) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+    public int firstOcc(int[] arr, int key, int lo, int hi) {
+        if(lo > hi && (lo < arr.length) && arr[lo] == key) return lo;
+        if(lo > hi) return -1;
+        int mid = (lo+hi)/2;
+        if(arr[mid] == key) {
+            int ans = mid;
+            ans = firstOcc(arr, key, lo, mid-1);
+            return ans;
+        } else if(arr[mid] < key) {
+            return firstOcc(arr, key, mid+1, hi);
+        } else {
+            return firstOcc(arr, key, lo, mid-1);
         }
-        return ans;
     }
-    public int lastOcc(int[] arr, int key) {
-        int n = arr.length;
-        int lo = 0;
-        int hi = arr.length-1;
-        int ans = -1;
-        while(lo <= hi) {
-            int mid = (lo + hi)/2;
-            if(arr[mid] == key) {
-                ans = mid;
-                lo = mid + 1;
-            } else if(arr[mid] < key) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+    public int lastOcc(int[] arr, int key, int lo, int hi) {
+        if(lo > hi && (hi >= 0) && arr[hi] == key) return hi;
+        if(lo > hi) return -1;
+        
+        int mid = (lo+hi)/2;
+        if(arr[mid] == key) {
+            int ans = mid;
+            ans = lastOcc(arr, key, mid+1, hi);
+            return ans;
+        } else if(arr[mid] > key) {
+            return lastOcc(arr, key, lo, mid-1);
+        } else {
+            return lastOcc(arr, key, mid+1, hi);
         }
-        return ans;
     }
 }
