3e0ce1d33efea14cf2ca5a6a38f1b5f97cd173a6
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-17 14:26:40 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (50.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
+        if(n < 2) return n;
         
-        return (fib(n-1) + fib(n-2));
+        int[] ans = new int[n+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
     }
 }
