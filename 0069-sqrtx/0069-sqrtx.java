6ba8e06998ed795a6cdcc4d9a42689b4613771ba
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-17 23:25:19 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (76.74%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
         if(x == 0) return 0;
+        return binarySearch(x, 1, x);
         // int lo = 1;
         // int hi = x;
         // while(lo < hi) {
@@ -10,13 +11,28 @@ class Solution {
         //     else lo = mid+1;
         // }
         // return lo;
-        return newtonSqrt(x);
+        // return newtonSqrt(x);
     }
-    public int newtonSqrt(int n) {
-        long i = n;
-        while(i > n/i) {
-            i = (i+n/i)/2;
+    // public int newtonSqrt(int n) {
+    //     long i = n;
+    //     while(i > n/i) {
+    //         i = (i+n/i)/2;
+    //     }
+    //     return (int)i;
+    // }
+    
+    public int binarySearch(int x, int lo, int hi) {
+        if(lo >= hi) return lo;
+        
+        int mid = lo + (hi-lo)/2;
+        if(mid <= x/mid && (mid+1) > x/(mid+1)) {
+            return mid;
         }
-        return (int)i;
+        else if(mid > x/mid) {
+            return binarySearch(x, lo, mid);
+        }
+        else {
+            return binarySearch(x,mid+1,hi);
+        }
     }
 }
