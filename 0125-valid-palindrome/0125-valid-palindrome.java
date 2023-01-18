023f9711fab144134c068c3312d5a9e4888aed01
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-18 13:29:49 +0530

    Time: 21 ms (47.37%), Space: 50.1 MB (9.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        int i = 0;
-        int j = s.length() - 1;
-        while(i <= j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        int l = 0;
+        int r = s.length()-1; 
+        return recursiveApproach(s, l, r);
+    }
+    
+    public boolean recursiveApproach(String s, int l, int r) {
+        if(l > r) return true;
+        
+        if(s.charAt(l) != s.charAt(r)) return false;
+        l++;
+        r--;
+        return recursiveApproach(s, l, r);
     }
 }
