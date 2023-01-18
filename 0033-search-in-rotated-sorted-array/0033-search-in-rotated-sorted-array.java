339318870b5abdc6b0d307e9147fa2332f6ee430
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-18 12:21:22 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (87.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = getPivot(nums, nums.length);
+        int pivot = getPivot(nums, nums.length, 0, nums.length-1);
         if(target >= nums[pivot] && target <= nums[nums.length-1]) {
             return binarySearch(nums, pivot, nums.length-1, target);
         } else {
@@ -8,30 +8,47 @@ class Solution {
         }
     }
     
-    public int getPivot(int[] nums, int n) {
-        int lo = 0;
-        int hi = n-1;
-        while(lo < hi) {
-            int mid = (lo+hi)/2;
-            if(nums[mid] >= nums[0]) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
+    public int getPivot(int[] nums, int n, int lo, int hi) {
+        // int lo = 0;
+        // int hi = n-1;
+        // while(lo < hi) {
+        //     int mid = (lo+hi)/2;
+        //     if(nums[mid] >= nums[0]) {
+        //         lo = mid + 1;
+        //     } else {
+        //         hi = mid;
+        //     }
+        // }
+        // return lo;
+        if(lo >= hi) return lo;
+        
+        int mid = (lo+hi)/2;
+        if(nums[mid] >= nums[0]) {
+            return getPivot(nums, n, mid+1, hi);
+        } else {
+            return getPivot(nums, n, lo, mid);
         }
-        return lo;
     }
     
-    public int binarySearch(int[] nums, int s, int e, int target) {
-        int lo = s;
-        int hi = e;
+    public int binarySearch(int[] nums, int lo, int hi, int target) {
+//         int lo = s;
+//         int hi = e;
         
-        while(lo <= hi) {
-            int mid = (lo+hi)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) lo = mid+1;
-            else hi = mid-1;
+//         while(lo <= hi) {
+//             int mid = (lo+hi)/2;
+//             if(nums[mid] == target) return mid;
+//             else if(nums[mid] < target) lo = mid+1;
+//             else hi = mid-1;
+//         }
+//         return -1;
+        if(lo > hi) return -1;
+        
+        int mid = (lo+hi)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) {
+            return binarySearch(nums, mid+1, hi, target);
+        } else {
+            return binarySearch(nums, lo, mid-1, target);
         }
-        return -1;
     }
 }
