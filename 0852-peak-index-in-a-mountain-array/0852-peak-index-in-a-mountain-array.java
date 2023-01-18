2cebdbcf0d1bc0ac2068a67f6934856e26752430
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-01-18 11:54:02 +0530

    Time: 0 ms (100.00%), Space: 59.1 MB (83.06%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,15 +1,30 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+        // int lo = 0;
+        // int hi = arr.length - 1;
+        // while(lo < hi) {
+        //     int mid = (lo+hi)/2;
+        //     if(arr[mid] < arr[mid+1]) {
+        //         lo = mid + 1;
+        //     } else {
+        //         hi = mid;
+        //     }
+        // }
+        // return lo;
         int lo = 0;
-        int hi = arr.length - 1;
-        while(lo < hi) {
-            int mid = (lo+hi)/2;
-            if(arr[mid] < arr[mid+1]) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
+        int hi = arr.length-1;
+        int ans = recursiveApproach(arr, lo, hi);
+        return ans;
+    }
+    
+    public int recursiveApproach(int[] arr, int lo, int hi) {
+        if(lo >= hi) return lo;
+        
+        int mid = (lo+hi)/2;
+        if(arr[mid] < arr[mid+1]) {
+            return recursiveApproach(arr, mid+1, hi);
+        } else {
+            return recursiveApproach(arr, lo, mid);
         }
-        return lo;
     }
 }
