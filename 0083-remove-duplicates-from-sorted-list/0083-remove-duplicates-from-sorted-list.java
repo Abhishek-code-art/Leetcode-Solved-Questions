eac7228850572ae9ac42e91c13ba8c060824ac72
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-03-04 18:22:36 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (68.54%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -13,17 +13,26 @@ class Solution {
         if(head == null || head.next == null) return head;
         
         ListNode curr = head;
-        ListNode next = head.next;
-        while(next != null) {
-            if(curr.val == next.val) {
-                while(next != null && curr.val == next.val) {
-                    next = next.next;
-                }
-                curr.next = next;
-            }
-            curr = next;
-            if(next != null) {
-                next = next.next;    
+        // ListNode next = head.next;
+        // while(next != null) {
+        //     if(curr.val == next.val) {
+        //         while(next != null && curr.val == next.val) {
+        //             next = next.next;
+        //         }
+        //         curr.next = next;
+        //     }
+        //     curr = next;
+        //     if(next != null) {
+        //         next = next.next;    
+        //     }
+        // }
+        
+        while(curr != null) {
+            if(curr.next != null && curr.val == curr.next.val) {
+                ListNode nxt_nxt = curr.next.next;
+                curr.next = nxt_nxt;
+            } else {
+                curr = curr.next;
             }
         }
         return head;
