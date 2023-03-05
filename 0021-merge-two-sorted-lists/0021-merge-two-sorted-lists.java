84aed63440b8ecbdbf392723b86b004b28d4f488
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-03-05 20:02:40 +0530

    Time: 0 ms (100.00%), Space: 42 MB (49.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,44 +8,55 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// if(list1 == null && list2 == null) return list1;
+        
+//         if(list1 == null && list2 != null) {
+//             return list2;
+//         }
+//         if(list1 != null && list2 == null) {
+//             return list1;
+//         }
+        
+//         ListNode ans = new ListNode(-101);
+//         ListNode head = ans;
+//         while(list1 != null && list2 != null) {
+//             if(list1.val <= list2.val) {
+//                 ans.next = list1;
+//                 ans = list1;
+//                 list1 = list1.next;
+//             } else {
+//                 ans.next = list2;
+//                 ans = list2;
+//                 list2 = list2.next;
+//             }
+//         }
+        
+//         while(list1 != null) {
+//             ans.next = list1;
+//             ans = list1;
+//             list1 = list1.next;
+//         }
+        
+//         while(list2 != null) {
+//             ans.next = list2;
+//             ans = list2;
+//             list2 = list2.next;
+//         }
+        
+//         head = head.next;
+//         return head;
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return list1;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
         
-        if(list1 == null && list2 != null) {
-            return list2;
+        if(l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        if(list1 != null && list2 == null) {
-            return list1;
-        }
-        
-        ListNode ans = new ListNode(-101);
-        ListNode head = ans;
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                ans.next = list1;
-                ans = list1;
-                list1 = list1.next;
-            } else {
-                ans.next = list2;
-                ans = list2;
-                list2 = list2.next;
-            }
-        }
-        
-        while(list1 != null) {
-            ans.next = list1;
-            ans = list1;
-            list1 = list1.next;
-        }
-        
-        while(list2 != null) {
-            ans.next = list2;
-            ans = list2;
-            list2 = list2.next;
-        }
-        
-        head = head.next;
-        return head;
     }
 }
