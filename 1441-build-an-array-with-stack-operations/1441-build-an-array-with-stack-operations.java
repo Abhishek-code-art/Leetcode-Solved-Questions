a3389e56bf427d6f3389b29dcda92fa612c3f267
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-03-10 22:48:16 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (26.44%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,34 +1,13 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> ans = new ArrayList<>();
-        int[] stack = new int[target.length];
-        int top = -1;
-        
-        for(int  i = 1; i <= n; i++) {
-            if((stack.length-top) > 1) {
-                top = push(ans, i, stack, top);
-            }
-            if(top >= 0 && target[top] != stack[top]) {
-                top = pop(ans, stack, top);
-            }
-            if(top >= 0 && target[top] == stack[top]) {
-                if(top == stack.length-1) return ans;
-            }
+        int j = 0;
+        for(int i = 1; i <= n && j < target.length; i++) {
+            ans.add("Push");
+            if(target[j] == i) j++;
+            else ans.add("Pop");
         }
         return ans;
-    }
-    
-    public int push(List<String> ans, int i, int[] stack, int top) {
-        top++;
-        stack[top] = i;
-        ans.add("Push");
-        return top;
-    }
-    
-    public int pop(List<String> ans, int[] stack, int top) {
-        ans.add("Pop");
-        top--;
-        return top;
     }
     
 }
