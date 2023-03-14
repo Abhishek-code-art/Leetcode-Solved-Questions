a461324c97727dbd98b156ab36cb19ecf0cdc141
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-03-14 21:06:21 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -35,45 +35,64 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(int M[][], int n)
     {
-    	Stack<Integer> st = new Stack<>();
-    	for(int i = 0; i < n; i++) {
-    	    st.push(i);
-    	}
+        
+        for(int i = 0; i < n; i++) {
+            boolean check = true;
+            for(int col = 0; col < n; col++) {
+                if(M[i][col] == 1) {
+                    check = false;
+                }
+            }
+            
+            for(int row = 0; row < n; row++) {
+                if(row == i) continue;
+                if(M[row][i] == 0) {
+                    check = false;
+                }
+            }
+            
+            if(check) return i;
+        }
+        return -1;
+    // 	Stack<Integer> st = new Stack<>();
+    // 	for(int i = 0; i < n; i++) {
+    // 	    st.push(i);
+    // 	}
     	
-    	while(st.size() > 1) {
-    	    int person1 = st.peek();
-    	    st.pop();
-    	    int person2 = st.peek();
-    	    st.pop();
+    // 	while(st.size() > 1) {
+    // 	    int person1 = st.peek();
+    // 	    st.pop();
+    // 	    int person2 = st.peek();
+    // 	    st.pop();
     	    
-    	    if(knows(M, person1, person2)) {
-    	        st.push(person2);
-    	    } else {
-    	        st.push(person1);
-    	    }
-    	}
-    	int candidate = st.peek();
+    // 	    if(knows(M, person1, person2)) {
+    // 	        st.push(person2);
+    // 	    } else {
+    // 	        st.push(person1);
+    // 	    }
+    // 	}
+    // 	int candidate = st.peek();
     	
-    	boolean rowCheck = true;
-    	for(int i = 0; i < n; i++) {
-    	    if(M[candidate][i] == 1) {
-    	        rowCheck = false;
-    	    }
-    	}
+    // 	boolean rowCheck = true;
+    // 	for(int i = 0; i < n; i++) {
+    // 	    if(M[candidate][i] == 1) {
+    // 	        rowCheck = false;
+    // 	    }
+    // 	}
     	
-    	boolean colCheck = true;
-    	for(int i = 0; i < n; i++) {
-    	    if(i == candidate) continue;
-    	    if(M[i][candidate] == 0) {
-    	        colCheck = false;
-    	    }
-    	}
+    // 	boolean colCheck = true;
+    // 	for(int i = 0; i < n; i++) {
+    // 	    if(i == candidate) continue;
+    // 	    if(M[i][candidate] == 0) {
+    // 	        colCheck = false;
+    // 	    }
+    // 	}
     	
-    	if(rowCheck && colCheck) {
-    	    return candidate;
-    	} else {
-    	    return -1;
-    	}
+    // 	if(rowCheck && colCheck) {
+    // 	    return candidate;
+    // 	} else {
+    // 	    return -1;
+    // 	}
     }
     
     private boolean knows(int[][] m, int A, int B) {
