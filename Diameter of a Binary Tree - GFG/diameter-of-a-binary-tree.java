f6154f691c5308276547d0ed24412b388dc60a40
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-03-25 12:49:26 +0530

    Added solution - LeetHub

---
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.java
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            System.out.println(g.diameter(root));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}*/
+
+class Solution {
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node root) {
+        return diameterAndHeight(root).diameter;
+    }
+    
+    private DiaPair diameterAndHeight(Node root) {
+        if(root == null) {
+            DiaPair p = new DiaPair(0, 0);
+            return p;
+        }
+        
+        DiaPair left = diameterAndHeight(root.left);
+        DiaPair right = diameterAndHeight(root.right);
+        
+        int leftDiameter = left.diameter;
+        int rightDiameter = right.diameter;
+        int leftToRight = left.height+ right.height+ 1;
+        
+        DiaPair ans = new DiaPair();
+        ans.diameter = Math.max(leftDiameter, Math.max(rightDiameter, leftToRight));
+        ans.height = Math.max(left.height, right.height) + 1;
+        
+        return ans;
+    }
+}
+class DiaPair {
+    int diameter;
+    int height;
+    
+    DiaPair(){}
+    
+    DiaPair(int diameter, int height) {
+        this.diameter = diameter;
+        this.height = height;
+    }
+}
+
+
