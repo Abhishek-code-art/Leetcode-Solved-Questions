bb8eb94737796410b9c1369a94d0c52f7ac322dc
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-05-27 18:04:32 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -37,26 +37,38 @@ class Solution{
 
 
     static Boolean isSubsetSum(int N, int arr[], int sum){
-        boolean[][] dp = new boolean[N+1][sum+1];
-        
-        for(int i = 0; i <= N; i++) {
-            dp[i][0] = true;
+        int[][] dp = new int[N+1][sum+1];
+        for(int[] num : dp) {
+            Arrays.fill(num, -1);
+        }
+        if(solve(arr, sum, N, dp) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    private static int solve(int[] arr, int sum, int n, int[][] dp) {
+        if(sum == 0) {
+            return dp[n][0] = 1;
         }
         
-        for(int j = 1; j <= sum; j++) {
-            dp[0][j] = false;
-        }
+        if(n <= 0) return 0;
         
         
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= sum; j++) {
-                if(arr[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
+        if(dp[n][sum] != -1) return dp[n][sum];
+        
+        if(arr[n-1] <= sum) {
+            boolean ans = (solve(arr, sum - arr[n-1], n-1, dp) == 1) || (solve(arr, sum, n-1, dp) == 1);
+            if(ans) {
+                dp[n][sum] = 1;
+                return 1;
+            } else {
+                dp[n][sum] = 0;
+                return 0;
             }
+        } else {
+            return dp[n][sum] = solve(arr, sum, n-1, dp);
         }
-        return dp[N][sum];
     }
 } 
