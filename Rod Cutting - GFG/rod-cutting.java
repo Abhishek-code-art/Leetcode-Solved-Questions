4e98c7ccd525a4fb4cc34618d502709a7f5bfd96
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-06-06 20:04:16 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -29,28 +29,39 @@ class Solution{
     private int max;
     public int cutRod(int price[], int n) {
         int[][] dp = new int[price.length+1][n+1];
-        for(int[] arr : dp) {
-            Arrays.fill(arr, -1);
-        }
         int[] length = new int[price.length];
         for(int i = 1; i <= n; i++) {
             length[i-1] = i;
         }
-        return solve(price, length, n, n, dp);
-    }
-    
-    private int solve(int[] price, int[] length, int n, int i, int[][] dp) {
-        if(n == 0 || i == 0) return 0;
-        if(dp[i][n] != -1) return dp[i][n];
         
-        int notTake = 0 + solve(price, length, n, i-1, dp);
-        int take = Integer.MIN_VALUE;
-        if(length[i-1] <= n) {
-            take = price[i-1]+solve(price, length, n-length[i-1], i, dp);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(length[i-1] <= j) {
+                    int inc = price[i-1] + dp[i][j-length[i-1]];
+                    int exc = dp[i-1][j];
+                    
+                    dp[i][j] = Math.max(inc, exc);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }   
+            }
         }
         
-        dp[i][n] = Math.max(take, notTake);
-        
-        return dp[i][n];
+        return dp[n][n];
     }
+    
+    // private int solve(int[] price, int[] length, int n, int i, int[][] dp) {
+    //     if(n == 0 || i == 0) return 0;
+    //     if(dp[i][n] != -1) return dp[i][n];
+        
+    //     int notTake = solve(price, length, n, i-1, dp);
+    //     int take = Integer.MIN_VALUE;
+    //     if(length[i-1] <= n) {
+    //         take = price[i-1]+solve(price, length, n-length[i-1], i, dp);
+    //     }
+        
+    //     dp[i][n] = Math.max(take, notTake);
+        
+    //     return Math.max(take, notTake);
+    // }
 }
