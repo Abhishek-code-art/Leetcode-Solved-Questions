143926bb20a8ffefe93ffd9504fd96aa8316fd74
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-07-23 23:03:15 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.java
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.java
@@ -39,27 +39,19 @@ class GFG {
 
 class Solution
 {
-    static int[][] dp;
     //Function to find length of shortest common supersequence of two strings.
     public static int shortestCommonSupersequence(String X,String Y,int m,int n)
     {
-        dp = new int[m+1][n+1];
-        for(int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(X.charAt(i-1) == Y.charAt(j-1)) {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
         }
-        
-        return m+n-solve(X, Y, m, n);
-    }
-    
-    private static int solve(String x, String y, int n, int m) {
-        if(n == 0 || m == 0) return 0;
-        
-        if(dp[n][m] != -1) return dp[n][m];
-        
-        if(x.charAt(n-1) == y.charAt(m-1)) {
-            return dp[n][m] = 1 + solve(x,y,n-1,m-1);
-        } else {
-            return dp[n][m] = Math.max(solve(x,y,n-1,m), solve(x,y,n,m-1));
-        }
+        return m+n-dp[m][n];
     }
 }
