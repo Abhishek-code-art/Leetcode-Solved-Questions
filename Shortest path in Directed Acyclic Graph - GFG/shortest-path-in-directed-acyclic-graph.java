bd043bb8df94577eef3b74796c3c0f3819683dbf
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-08-03 13:52:50 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
@@ -30,73 +30,68 @@ class Main {
 
 //User function Template for Java
 class Solution {
+    
+    private void dfs(int node, boolean[] visit, ArrayList<ArrayList<Pair>> adj, Stack<Integer> st) {
+        visit[node] = true;
+        for(Pair p : adj.get(node)) {
+            if(!visit[p.node]) {
+                dfs(p.node, visit, adj, st);
+            }
+        }
+        st.push(node);
+    }
 
 	public int[] shortestPath(int N,int M, int[][] edges) {
-		ArrayList<ArrayList<Node>> adj = new ArrayList<>();
+		ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 		for(int i = 0; i < N; i++) {
-		    adj.add(new ArrayList<Node>());
+		    adj.add(new ArrayList<Pair>());
 		}
 		
 		for(int i = 0; i < M; i++) {
 		    int u = edges[i][0];
 		    int v = edges[i][1];
-		    int w = edges[i][2];
+		    int d = edges[i][2];
 		    
-		    Node node = new Node(v, w);
-		    adj.get(u).add(node);
+		    Pair p = new Pair(v,d);
+		    adj.get(u).add(p);
 		}
 		
-		
-		boolean[] visited = new boolean[N];
 		Stack<Integer> st = new Stack<>();
-		
+		boolean[] visit = new boolean[N];
 		for(int i = 0; i < N; i++) {
-		    if(!visited[i]) {
-		        dfs(i, adj, visited, st);
+		    if(!visit[i]) {
+		        dfs(i,visit,adj,st);
 		    }
 		}
 		
 		int[] dist = new int[N];
-		Arrays.fill(dist, Integer.MAX_VALUE);
+		Arrays.fill(dist,Integer.MAX_VALUE);
 		dist[0] = 0;
-		
+	
 		while(!st.isEmpty()) {
 		    int top = st.pop();
 		    if(dist[top] != Integer.MAX_VALUE) {
-		        for(Node i : adj.get(top)) {
-		            if(dist[top] + i.weight < dist[i.value]) {
-		                dist[i.value] = dist[top] + i.weight;
-		            }
-		        }
+    		    ArrayList<Pair> nbrs = adj.get(top);
+    		    for(Pair nbr : nbrs) {
+    		        if(dist[top]+nbr.dist < dist[nbr.node]) {
+    		            dist[nbr.node] = dist[top]+nbr.dist;
+    		        }
+    		    }
 		    }
 		}
 		
 		for(int i = 0; i < N; i++) {
-		    if(dist[i] == Integer.MAX_VALUE) {
-		        dist[i] = -1;
-		    }
+		    if(dist[i] == Integer.MAX_VALUE) dist[i] = -1;
 		}
-		
 		return dist;
-	}
-	
-	private void dfs(int node, ArrayList<ArrayList<Node>> adj, boolean[] visited, Stack<Integer> st) {
-	    visited[node] = true;
-	    for(Node neighbour : adj.get(node)) {
-	        if(!visited[neighbour.value]) {
-	            dfs(neighbour.value, adj, visited, st);
-	        }
-	    }
-	    st.push(node);
 	}
 }
 
-class Node {
-    int value;
-    int weight;
-    
-    Node(int value, int weight) {
-        this.value = value;
-        this.weight = weight;
+class Pair {
+    int node;
+    int dist;
+    Pair(int n, int d) {
+        node = n;
+        dist = d;
     }
 }
