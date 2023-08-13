0881ef58c5a345e6f508a39ef146d920b027980e
Abhishek <80666490+Abhishek-code-art@users.noreply.github.com>
2023-08-13 21:00:30 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.java
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.java
@@ -8,7 +8,7 @@ class GFG {
             new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
-            long n = Integer.parseInt(read.readLine());
+            int n = Integer.parseInt(read.readLine());
             Solution ob = new Solution();
 
             System.out.println(ob.nthFibonacci(n));
@@ -22,16 +22,28 @@ class GFG {
 
 //User function Template for Java
 class Solution {
-    private static long m = 1000000007;
-    static long nthFibonacci(long n){
-        long[] dp = new long[(int)(n+1)];
+    static long[] dp;
+    static long m = 1000000007;
+    static int nthFibonacci(int n){
+        if(n == 0) return 0;
+        if(n <= 2) return 1;
+        
+        dp = new long[n+1];
         dp[0] = 0;
-        dp[1] = 1;
+        dp[1] = dp[2] = 1;
         
-        for(long i = 2; i <= n; i++) {
-            dp[(int)i] = (dp[(int)i-1]%m + dp[(int)i-2]%m)%m;
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1]%m + dp[i-2]%m)%m;
         }
-        
-        return dp[(int)n]%m;
+        return (int)(dp[n]%m);
     }
+    
+    // private static int solve(int n) {
+    //     if(n == 0) return 0;
+    //     if(n == 1) return 1;
+        
+    //     if(dp[n] != -1) return dp[n];
+        
+    //     return dp[n] = solve(n-1) + solve(n-2);
+    // }
 }
